0f82c4f963aea7f4a51ef41b36133703b20763c4
Jacob Darvin <jacobisdarvin@gmail.com>
2023-05-01 14:28:37 +0800

    Create PrimeNumber.java

---
+++ day2/PrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class PrimeNumber {
+  public static boolean checkIfPrime(int n) {
+    boolean isPrime = true;
+    for(int i = 2; i <= n/2; i++) {
+      if(n % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    return isPrime;
+  }
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    System.out.print("Enter number to check if prime: ");
+
+    int nInput = myScanner.nextInt();
+    System.out.println("Is your number a prime? " + checkIfPrime(nInput));
+
+    myScanner.close();
+  }
+}
