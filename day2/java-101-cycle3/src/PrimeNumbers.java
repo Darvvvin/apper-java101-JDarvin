c677e9412a1a0e9e7e480c76ebf26ca6517c6837
Jacob Darvin <jacobisdarvin@gmail.com>
2023-05-02 13:07:04 +0800

    Update PrimeNumbers.java

--- day2/java-101-cycle3/src/PrimeNumbers.java
+++ day2/java-101-cycle3/src/PrimeNumbers.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter number to check if prime: ");
+
+        int nInput = myScanner.nextInt();
+        System.out.println("Is your number a prime? " + checkIfPrime(nInput));
+
+        myScanner.close();
+    }
+
+    public static boolean checkIfPrime(int n) {
+        boolean isPrime = true;
+        for(int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
     }
 }
