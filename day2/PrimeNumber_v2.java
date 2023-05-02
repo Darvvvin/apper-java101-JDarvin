a207d5e6df6491a90efd452a8b6f653463c968df
Jacob Darvin <jacobisdarvin@gmail.com>
2023-05-02 13:34:37 +0800

    Run for loop only if n is >= 2

--- day2/PrimeNumber_v2.java
+++ day2/PrimeNumber_v2.java
@@ -1,21 +1,26 @@
 public class PrimeNumber_v2 {
   public static void main(String[] args) {
-    System.out.println("isPrime? " + isPrimeNumber(11));
+    System.out.println("isPrime? " + isPrimeNumber(7));
   }
 
   public static boolean isPrimeNumber(int n) {
     long startTime = System.nanoTime();
-
     boolean isPrime = true;
-    for (int i = 2; i <= n / 2; i++) {
-      if (n % i == 0) {
-        isPrime = false;
-        break;
+
+    if (n >= 2) {
+
+      for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+          isPrime = false;
+          break;
+        }
       }
+
+    } else {
+      isPrime = false;
     }
 
     long endTime = System.nanoTime();
-
     System.out.println("Duration: " + (endTime - startTime));
 
     return isPrime;
