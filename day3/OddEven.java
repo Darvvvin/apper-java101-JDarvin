fa2e2a69ca2ff58f1b0716ec9d08b5388a6c2bdf
Jacob Darvin <jacobisdarvin@gmail.com>
2023-05-04 11:13:10 +0800

    Create OddEven.java

---
+++ day3/OddEven.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 234, 2, 4, 56, 17, 50, 1235, 6, 1, 23, 48, 47, 34};
+        int oddCount = 0;
+        int evenCount = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Odd: " + oddCount);
+        System.out.println("Even: " + evenCount);
+    }    
+}
