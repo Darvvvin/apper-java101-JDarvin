a37e4e907237222f17ef04644b35c010c2fa5ede
Jacob Darvin <jacobisdarvin@gmail.com>
2023-05-12 10:31:29 +0800

    Finished Java capstone

--- finalproject/gcash/src/App.java
+++ finalproject/gcash/src/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, String> users = new HashMap<>();
+        Map<String, User> users = new HashMap<>();
 
-        users.put("09175861661", "Bob");
-        users.put("09175861662", "Marley");
-        users.put("09175861663", "Seth");
-        users.put("09175861664", "Ryan");
-        users.put("09175861665", "Fritz");
+        users.put("09175861661", new User("Bob", 100));
+        users.put("09175861662", new User("Marley", 100));
+        users.put("09175861663", new User("Seth", 100));
+        users.put("09175861664", new User("Ryan", 100));
+        users.put("09175861665", new User("Fritz", 100));
 
         System.out.println("**Register an Account**");
         System.out.print("Number: ");
@@ -30,8 +30,41 @@ public class App {
             System.exit(0);
         }
 
-        users.put(numberInput, nameInput);
+        users.put(numberInput, new User(nameInput, 100));
 
         System.out.println("\n**Share a Load**");
+        System.out.print("Sender mobile number: ");
+        String senderNumberInput = scanner.nextLine();
+
+        if(users.get(senderNumberInput) == null) {
+            System.out.println("Sender " + senderNumberInput + " not found!");
+            System.exit(0);
+        }
+
+        System.out.print("Recipient mobile number: ");
+        String recipientNumberInput = scanner.nextLine();
+
+        if(users.get(recipientNumberInput) == null) { //TODO: Create method to check if number input is valid
+            System.out.println("Recipient " + recipientNumberInput + " not found!");
+            System.exit(0);
+        } else if (recipientNumberInput.equals(senderNumberInput)) {
+            System.out.println("Sender and recipient shouldn't be the same!");
+            System.exit(0);
+        }
+
+        System.out.print("Enter amount (Enter a valid number): ");
+        double amountInput = scanner.nextDouble();
+        if(users.get(senderNumberInput).getBalance() < amountInput) {
+            System.out.println("Insufficient load transfer!");
+        } else {
+            users.get(senderNumberInput).setBalance(users.get(senderNumberInput).getBalance() - amountInput);
+            users.get(recipientNumberInput).setBalance(users.get(recipientNumberInput).getBalance() + amountInput);
+            System.out.println("Amount of " + amountInput + " was SUCCESSFULLY loaded into " + recipientNumberInput + " (" + users.get(recipientNumberInput).getName() + ")" + " from " + senderNumberInput + " (" + users.get(senderNumberInput).getName() + ")\n");
+        }
+
+        System.out.println("**Current Balances**");
+        for (Map.Entry<String, User> entry : users.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().getName() + " " + entry.getValue().getBalance());
+        }
     }
 }
---
+++ finalproject/gcash/src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private String name;
+    private double balance;
+
+    public User(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
